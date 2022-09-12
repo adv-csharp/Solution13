000f056377d79b0e88958f683b60bfa8269cadde
Pasha <pashaie@gmail.com>
2022-09-12 18:41:11 +0430

    sqlite

---
+++ 2_sqlite/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-templa te for more information
+
+/*
+ *  ADO.net
+ *  Entity Framework
+ **/
+
+
+using System.Data.SQLite;
+
+var connStr = "URI=file:app.db";
+using var con = new SQLiteConnection(connStr);
+con.Open();
+
+using var  command = new SQLiteCommand("select * from product");
+command.Connection = con;
+
+var dr = command.ExecuteReader();
+while (dr.Read())
+{
+    Console.WriteLine(dr["Name"] + ": " + dr["Price"]);
+
+}
+
+
+Console.WriteLine("Hello, World!");
