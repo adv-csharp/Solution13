6d2afa7f7e685600e5f4416b93007c316d3cfc0d
Pasha <pashaie@gmail.com>
2022-09-12 18:22:46 +0430

    sms & email

---
+++ 1_email_and_sms/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace _1_email_and_sms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMEssage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSendSMS = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To:";
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(75, 12);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(334, 23);
+            this.textBoxTo.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message";
+            // 
+            // textBoxMEssage
+            // 
+            this.textBoxMEssage.Location = new System.Drawing.Point(75, 70);
+            this.textBoxMEssage.Multiline = true;
+            this.textBoxMEssage.Name = "textBoxMEssage";
+            this.textBoxMEssage.Size = new System.Drawing.Size(334, 151);
+            this.textBoxMEssage.TabIndex = 2;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(334, 240);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(75, 41);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(334, 23);
+            this.textBoxSubject.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subject";
+            // 
+            // buttonSendSMS
+            // 
+            this.buttonSendSMS.Location = new System.Drawing.Point(334, 335);
+            this.buttonSendSMS.Name = "buttonSendSMS";
+            this.buttonSendSMS.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendSMS.TabIndex = 6;
+            this.buttonSendSMS.Text = "Send SMS";
+            this.buttonSendSMS.UseVisualStyleBackColor = true;
+            this.buttonSendSMS.Click += new System.EventHandler(this.buttonSendSMS_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 297);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "To:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 370);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSendSMS);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMEssage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxTo;
+        private Label label2;
+        private TextBox textBoxMEssage;
+        private Button buttonSend;
+        private TextBox textBoxSubject;
+        private Label label3;
+        private Button buttonSendSMS;
+        private TextBox textBox1;
+        private Label label4;
+    }
+}
---
+++ 1_email_and_sms/Form1.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace _1_email_and_sms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            /*
+             * Email install konim - exchange server,
+             *  SMTP
+             *  POP3
+             *  IMAP
+             *
+             *  https://www.google.com/settings/security/lesssecureapps
+             *
+             */
+            string username = "fad.csharp@gmail.com";
+            string password = "!p*Dx#r!AE%TZtS3NEu42U3TG";
+
+
+            var message = new MailMessage();
+            message.Subject = textBoxSubject.Text;
+            message.To.Add(textBoxTo.Text);
+            message.Body = textBoxMEssage.Text;
+            message.From = new MailAddress(username);
+            message.IsBodyHtml = true;
+            message.Attachments.Add(new Attachment("c:/1.txt"));
+
+            var smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(username, password);
+
+            smtpClient.Send(message);
+            MessageBox.Show("Success");
+        }
+
+        async private void   buttonSendSMS_Click(object sender, EventArgs e)
+        {
+            /*
+             * SMS
+             *  Modem GSM
+             *  SMS Provider
+             *  sms.ir
+             *
+             *
+             *  RestSharp
+             * httpClient
+             */
+
+            var username = "";
+            var password = "";
+            var lineNo = "";
+
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync(
+                $"https://ip.sms.ir/SendMessage.ashx?user={username}&pass={password}&text=khoshamadid&to={textBox1.Text}&lineNo={lineNo}",
+                new StringContent(""));
+        }
+    }
+}
